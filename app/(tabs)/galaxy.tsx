a2835237130fd39f3e25aef0413009234e1f7980
Diego <diego.gandia.sahuquillo@gmail.com>
2025-07-24 00:25:53 +0200

    Cambio de status bar

--- app/(tabs)/galaxy.tsx
+++ app/(tabs)/galaxy.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import GalaxyComponent from "../../components/main/GalaxyComponent";
 import ResourceBar from "../../components/secondary/ResourceBar";
 
